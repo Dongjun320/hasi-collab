24adb276fce85ab963c3b2075e575a289581e164
DESKTOP-03B16KV\administer <jhkim966@gmail.com>
2026-05-19 09:54:23 +0900

    securityconfig, jwtprovider 생성하고 패키지 구분

---
+++ service/src/main/java/com/hasi/service/jwt/jwtProvider.java
@@ -0,0 +1,15 @@
+package com.hasi.service.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class jwtProvider {
+    @Value("${jwt.secret}")
+    private String secret;
+}
