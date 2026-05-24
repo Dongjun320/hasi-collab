74bbdc562f156546345e2494327dea3a9e2e4c17
Jinwoo Jeong <kosmostreeplus@proton.me>
2026-05-24 18:55:47 +0900

    Redis/PostgreSQL 연결 확인, Message Entity 생성

--- service/src/main/java/com/hasi/service/message/Message.java
+++ service/src/main/java/com/hasi/service/message/Message.java
@@ -1,5 +1,49 @@
 package com.hasi.service.message;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.time.LocalDateTime;
+
+/**
+ * Message Entity 입니다.
+ * @author Jinwoo Jeong
+ */
+@Entity
+@Table(name = "messages")
+@Getter
+@Setter
+@ToString
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long channelId;
+    private Long dmRoomId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_msg_id")
+    private Message parentMessage;
+
+//    @Column(nullable = false, columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+//    @Column(nullable = false)
+//    private Long senderId;
+//
+//
+//    @Column(nullable = false)
+//    private Boolean isDeleted = false;
+//
+//    private LocalDateTime deletedAt;
+//
+//    @Column(nullable = false)
+//    private LocalDateTime createdAt;
+//
+//    @Column(nullable = false)
+//    private LocalDateTime updatedAt;
 }
