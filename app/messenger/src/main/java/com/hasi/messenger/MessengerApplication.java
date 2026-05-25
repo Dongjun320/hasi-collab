c134bdbd3fc6e6acdbd7e85d00952101055fbf8b
Jinwoo Jeong <kosmostreeplus@proton.me>
2026-05-25 20:56:17 +0900

    Messenger는 Redis 드랍. 기본 in-memory 브로커만 사용, DB에서 메시지 가져옴

--- app/messenger/src/main/java/com/hasi/messenger/MessengerApplication.java
+++ app/messenger/src/main/java/com/hasi/messenger/MessengerApplication.java
@@ -13,11 +13,6 @@ public class MessengerApplication {
 	public static void main(String[] args) {
 		var ctx = SpringApplication.run(MessengerApplication.class, args);
 
-		// Redis 연결 테스트
-		StringRedisTemplate redis = ctx.getBean(StringRedisTemplate.class);
-		redis.opsForValue().set("test-key", "헬로 Redis");
-		System.out.println("Redis 조회: " + redis.opsForValue().get("test-key"));
-
 		// PostgreSQL 연결 테스트
 		MessageRepository repo = ctx.getBean(MessageRepository.class);
 		Message msg = new Message();
