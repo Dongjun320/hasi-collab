e58b4c0073ce11143e743d6556527b3296f4c30c
jj441 <55660417+jj441@users.noreply.github.com>
2026-07-20 19:51:35 +0900

    누락 MemberService 추가
    누락 MemberService 추가

---
+++ app/service/src/main/java/com/hasi/service/workspace/member/MemberService.java
@@ -0,0 +1,14 @@
+package com.hasi.service.workspace.member;
+
+import com.hasi.collab.model.WorkspaceMemberData;
+import com.hasi.collab.model.WorkspaceMemberInviteRequest;
+import com.hasi.service.workspace.member.entity.WorkspaceMember;
+import com.hasi.service.workspace.member.repository.WorkspaceMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+}
