7e05b0a692a51365f310a35a03501a68d01aeec5
KIM JIHWAN <jhkim966@gmail.com>
2026-07-23 19:47:36 +0900

    친구 추가 요청/수락/거절 추가, seed sql 주석 수정

--- app/service/src/main/java/com/hasi/service/friend/entity/Friend.java
+++ app/service/src/main/java/com/hasi/service/friend/entity/Friend.java
@@ -1,5 +1,6 @@
 package com.hasi.service.friend.entity;
 
+import com.hasi.service.auth.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,7 +10,7 @@ import java.time.LocalDateTime;
 @Table(
         name = "friends",
         uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"user_id", "friend_id"})
+                @UniqueConstraint(columnNames = {"sender_id", "receiver_id"})
         }
 )
 @Getter
@@ -22,17 +23,48 @@ public class Friend {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @Column(name = "sender_id", nullable = false)
+    private Long senderId;      // 요청 보낸 사람
 
-    @Column(name = "friend_id", nullable = false)
-    private Long friendId;
+    @Column(name = "receiver_id", nullable = false)
+    private Long receiverId;    // 요청 받은 사람
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private Status status = Status.PENDING;
+
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void accept() {
+        this.status = Status.ACCEPTED;
+    }
+
+    public void reject() {
+        this.status = Status.REJECTED;
+    }
+
+    public enum Status {
+        PENDING, ACCEPTED, REJECTED
+    }
+
+    public void resendAsSender(Long senderId, Long receiverId) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.status = Status.PENDING;
     }
 }
